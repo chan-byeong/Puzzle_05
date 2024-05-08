f89eed4a7945dd1e97638bdd22077846738553c3
byeong <qudcks4885@gmail.com>
2024-05-08 22:53:16 +0900

    feat: 라우터 path 및 element 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./Pages/Root";
+import HomePage from "./Pages/HomePage";
+import MateHome from "./components/Mate/MateHome";
 
 const router = createBrowserRouter([
   {
@@ -8,7 +10,29 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <h1>Root Page</h1>,
+        element: <HomePage />,
+      },
+      {
+        path: "/mate",
+        element: <MateHome />,
+        children: [
+          {
+            path: "/mate/room-mate",
+            element: <h1>roommate</h1>,
+          },
+          {
+            path: "/mate/bob-mate",
+            element: <h1>BOB MATE</h1>,
+          },
+          {
+            path: "/mate/lang-exchange",
+            element: <h1>Language Exchange</h1>,
+          },
+          {
+            path: "/mate/others",
+            element: <h1>Others</h1>,
+          },
+        ],
       },
     ],
   },
