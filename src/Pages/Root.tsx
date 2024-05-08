daa2ca53a7ff484739b49f8c508585f536bfa9ed
byeong <qudcks4885@gmail.com>
2024-05-08 17:13:32 +0900

    feat: 전역 CSS 초기화 및 폰트 설정

--- src/Pages/Root.tsx
+++ src/Pages/Root.tsx
@@ -1,8 +1,14 @@
 import { Outlet } from "react-router-dom";
+import { css } from "@emotion/react";
 
 function Root() {
+  const mlayout = css`
+    font-family: "Pretendard Variable";
+    padding: 47px 16px 34px 16px;
+    min-height: 844px;
+  `;
   return (
-    <div>
+    <div css={mlayout}>
       <Outlet />
     </div>
   );
