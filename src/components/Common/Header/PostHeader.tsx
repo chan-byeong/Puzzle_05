cfad8c3b029269e098a4d61f7996f972d2fccd6a
byeong <qudcks4885@gmail.com>
2024-05-14 16:49:39 +0900

    feat: 네비게이터 등록

--- src/components/Common/Header/PostHeader.tsx
+++ src/components/Common/Header/PostHeader.tsx
@@ -1,9 +1,12 @@
 import * as H from "./Header.style";
+import { useNavigate } from "react-router-dom";
 
 function PostHeader() {
+  const nav = useNavigate();
+
   return (
     <H.HeaderBox style={{ paddingTop: "8px", marginBottom: "0px" }}>
-      <H.XIcon />
+      <H.XIcon onClick={() => nav(-1)} />
       <H.Title>글쓰기</H.Title>
       <H.MiddleTxt>임시저장</H.MiddleTxt>
     </H.HeaderBox>
--- src/components/Common/Header/RouteHeader.tsx
+++ src/components/Common/Header/RouteHeader.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import * as H from "./Header.style";
+import { useNavigate } from "react-router-dom";
 
 function RouteHeader() {
+  const nav = useNavigate();
+
   return (
     <H.HeaderBox style={{ marginBottom: "42px" }}>
-      <H.Back />
+      <H.Back onClick={() => nav(-1)} />
       <H.Title>게시판</H.Title>
       <H.Message />
     </H.HeaderBox>
