4d706d0276e746439c2726dd40a9142fe7bf2547
byeong <qudcks4885@gmail.com>
2024-05-27 14:22:25 +0900

    fix: 헤더 컴포넌트 title props 추가

--- src/components/Common/Header/PostHeader.tsx
+++ src/components/Common/Header/PostHeader.tsx
@@ -1,13 +1,17 @@
 import * as H from "./Header.style";
 import { useNavigate } from "react-router-dom";
 
-function PostHeader() {
+interface PostHeaderProps {
+  title: string;
+}
+
+function PostHeader({ title }: PostHeaderProps) {
   const nav = useNavigate();
 
   return (
     <H.HeaderBox style={{ paddingTop: "8px", marginBottom: "0px" }}>
       <H.XIcon onClick={() => nav(-1)} />
-      <H.Title>글쓰기</H.Title>
+      <H.Title>{title}</H.Title>
       <H.MiddleTxt>임시저장</H.MiddleTxt>
     </H.HeaderBox>
   );
