2ba598916012045b6f282fb116d9597f417b94b6
byeong <qudcks4885@gmail.com>
2024-05-30 19:21:41 +0900

    feat: ErrorBoundary 추가

--- src/Pages/SharePage.tsx
+++ src/Pages/SharePage.tsx
@@ -1,12 +1,15 @@
 import { Suspense } from "react";
 
 import Share from "../components/Share/Share";
+import ErrorBoundary from "../components/Common/ErrorBoundary";
 
 function SharePage() {
   return (
-    <Suspense fallback={<h1>Loading...</h1>}>
-      <Share />
-    </Suspense>
+    <ErrorBoundary fallback={<h1>Maybe Server is Off</h1>}>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Share />
+      </Suspense>
+    </ErrorBoundary>
   );
 }
 
