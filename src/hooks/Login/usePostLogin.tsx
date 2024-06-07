2bb03f3af8d56b97bac7f54824dfe530d2c1fb25
byeong <qudcks4885@gmail.com>
2024-06-07 17:18:44 +0900

    feat: 로그인 API

---
+++ src/hooks/Login/usePostLogin.tsx
@@ -0,0 +1,48 @@
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+
+import { BASE_URL } from "../Constans";
+
+interface LoginType {
+  email: string;
+  pw: string;
+}
+
+const requestLogin = async (data: LoginType) => {
+  const response = await fetch(`${BASE_URL}/member/login`, {
+    method: "post",
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+  });
+
+  if (response.status !== 200) {
+    throw new Error("Error occurs while requesting login");
+  }
+
+  const access_token = response.headers.get("Access_Token");
+  const refresh_token = response.headers.get("Refresh_Token");
+
+  if (access_token !== null) {
+    localStorage.setItem("access_token", access_token);
+    localStorage.setItem("refresh_token", access_token);
+  } else console.log(access_token, refresh_token, "access_token is null");
+
+  return response.json();
+};
+
+function usePostLogin() {
+  const nav = useNavigate();
+  const mutation = useMutation({
+    mutationKey: ["user-login"],
+    mutationFn: (data: LoginType) => requestLogin(data),
+    onSuccess: (data) => {
+      if (data.status === 200) nav("/");
+      else alert("로그인 실패: 학번,비번을 확인하세요");
+    },
+    onError: () => alert("네트워크 에러!"),
+  });
+  return mutation;
+}
+
+export { requestLogin, usePostLogin };
