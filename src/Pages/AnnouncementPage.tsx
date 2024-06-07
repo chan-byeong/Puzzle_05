0115165ae1d50f00b66987df8718a55a842988a5
byeong <qudcks4885@gmail.com>
2024-06-07 17:11:37 +0900

    feat: 데이터 패칭 로딩 인디케이터 추가

--- src/Pages/AnnouncementPage.tsx
+++ src/Pages/AnnouncementPage.tsx
@@ -1,7 +1,19 @@
+import { Suspense } from "react";
+
+import ErrorBoundary from "../components/Common/ErrorBoundary";
 import Announcement from "../components/Announcement/Announcement";
+import Loader from "../components/Common/Loader";
 
 function AnnouncementPage() {
-  return <Announcement />;
+  return (
+    <>
+      <ErrorBoundary fallback={<h1>Error While Fetching Notices</h1>}>
+        <Suspense fallback={<Loader />}>
+          <Announcement />
+        </Suspense>
+      </ErrorBoundary>
+    </>
+  );
 }
 
 export default AnnouncementPage;
---
+++ src/components/Common/Loader.tsx
@@ -0,0 +1,30 @@
+import { CSSProperties } from "react";
+import styled from "@emotion/styled";
+
+import DotLoader from "react-spinners/DotLoader";
+
+const override: CSSProperties = {
+  display: "block",
+  margin: "0 auto",
+  height: "",
+};
+
+function Loader() {
+  return (
+    <Container>
+      <DotLoader color="#00DD9B" loading={true} cssOverride={override} size={30} />
+    </Container>
+  );
+}
+
+export default Loader;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  min-height: 844px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
